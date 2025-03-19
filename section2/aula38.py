255ea73d85968a089ffe15adf2f7922acb9a7d2e
GuilhermeSRei <139374621+GuilhermeSRei@users.noreply.github.com>
2025-03-19 19:18:24 -0300

    funcao_decoradora

--- section2/aula38.py
+++ section2/aula38.py
@@ -4,5 +4,28 @@
 # Decoradores são usados para fazer o python
 # usar as funções decoradoras em outras funções.
 
+# Função decoradora
+def cria_funcao(func):
+    def interna(*args, **kwargs):
+        print('Vou te decorar')
+        for arg in args:
+            is_string(arg)
+
+        resultado = func(*args, **kwargs)
+        print('Ok, foi decorada')
+        return resultado
+    return interna
+
+#-----------------------------------------------------------
+
 def inverte_string(string):
     return string[::-1]
+
+
+def is_string(param):
+    if not isinstance(param, str):
+        raise TypeError('Param deve ser uma string')
+
+inverte_string_checando_parametro = cria_funcao(inverte_string)
+invertida = inverte_string_checando_parametro('Guilherme')
+print(invertida)
