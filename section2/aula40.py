b55adba67a056311b238861b52fe4a189cd6a311
GuilhermeSRei <139374621+GuilhermeSRei@users.noreply.github.com>
2025-03-20 15:52:41 -0300

    decoradores com parametro

--- section2/aula40.py
+++ section2/aula40.py
@@ -1,16 +1,26 @@
-def decoradora(func):
-    print('Decoradora 1')
+# Decoradores com parâmetros
 
-    def inner_func(*args, **kwargs):
-        print('Aninhada')
-        res =  func(*args, **kwargs)
-        return res
-    return inner_func
+def fabrica_de_decoradores(a, b, c):
+    def fabrica_de_funcoes(func):
+        print('Decoradora 1')
 
-@decoradora
+        def inner(*args, **kwargs):
+            print('Aninhada')
+            res =  func(*args, **kwargs)
+            return res
+        return inner
+    return fabrica_de_funcoes
+
+@fabrica_de_decoradores(1, 3, 5)
 def soma(x, y):
     return x + y
+
+multiplica = fabrica_de_decoradores(1, 2, 3)(lambda x,y : x * y)
 
 dez_mais_cinco = soma(10, 5)
 print(dez_mais_cinco)
 
+print()
+
+multiplica_dez_mais_cinco = multiplica(10, 5)
+print(multiplica_dez_mais_cinco)
