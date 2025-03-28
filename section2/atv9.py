2e5f46ebfe364f4612175795910a80959e6378f6
GuilhermeSRei <139374621+GuilhermeSRei@users.noreply.github.com>
2025-03-28 15:50:35 -0300

    Exercício - Lista de tarefas com desfazer e refazer

---
+++ section2/atv9.py
@@ -0,0 +1,94 @@
+# Exercício - Lista de tarefas com desfazer e refazer
+# Música para codar =)
+# Everybody wants to rule the world - Tears for fears
+# todo = [] -> lista de tarefas
+# todo = ['fazer café'] -> Adicionar fazer café
+# todo = ['fazer café', 'caminhar'] -> Adicionar caminhar
+# desfazer = ['fazer café',] -> Refazer ['caminhar']
+# desfazer = [] -> Refazer ['caminhar', 'fazer café']
+# refazer = todo ['fazer café']
+# refazer = todo ['fazer café', 'caminhar']
+
+
+
+def listar(tarefas):
+        print()
+        if not tarefas:
+           print('Nenhuma tarefa para listar')
+           return
+
+        print('Tarefas: ')
+        for tarefa in tarefas:
+             print(f'\t{tarefa}')
+        
+        print()
+
+def desfazer(tarefas, tarefas_refazer):
+    print()
+
+    if not tarefas:
+         print('Nenhuma tarefa para desfazer')
+         return
+
+    tarefa = tarefas.pop()
+    tarefas_refazer.append(tarefa)
+
+    print()
+
+def adicionar(tarefa, tarefas):
+    print()
+    tarefa = tarefa.strip()
+
+    if not tarefa:
+         print('Você não tem nenhuma tarefa')
+         return
+         
+    tarefas.append(tarefa)
+
+    print()
+
+
+
+def refazer(tarefas, tarefas_refazer):
+    print()
+
+    if not tarefas_refazer:
+         print('Nenhuma tarefa para refazer')
+         return
+
+    tarefa = tarefas_refazer.pop()
+    tarefas.append(tarefa)
+
+    print()
+
+tarefas = []
+tarefas_refazer = []
+
+while True:
+
+    print('Comandos: listar, desfazer e refazer')
+    tarefa = input('Digite uma tarefa ou comando: ')
+      
+    if tarefa == 'listar':
+        listar(tarefas)
+        continue
+    elif tarefa == 'desfazer':
+        desfazer(tarefas, tarefas_refazer)
+        continue
+    elif tarefa == 'refazer':
+         refazer(tarefas, tarefas_refazer)
+         continue
+    else:
+        adicionar(tarefa, tarefas)
+        listar(tarefas)
+        continue
+
+    
+    
+
+    
+    
+    
+    
+    
+
